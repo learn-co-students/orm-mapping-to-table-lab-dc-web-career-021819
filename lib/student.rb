2ae5fa5ab05888d9867ded367e26c1769e2e3698
github username <github email address>
2019-02-27 13:52:53 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,82 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.length = row[2]
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).collect do | row |
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).collect do | row |
+      self.new_from_db(row)
+    end
+  end
+
+  def self.all_students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).collect do | row |
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, num).collect do | row |
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).collect do | row |
+      self.new_from_db(row)
+    end 
+  end
+
   def save
     sql = <<-SQL
         INSERT INTO students (name, grade)
